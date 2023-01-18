f539ed60ad870c5580f9d20ff232b7baee134947
Ajmal Kottilingal <ajmal.kottilingal@transferwise.com>
2023-01-18 22:06:57 +0300

    first commit

---
+++ src/main/java/com/ajmalab/demo/DependencyDiffCheckApplication.java
@@ -0,0 +1,13 @@
+package com.ajmalab.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyDiffCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependencyDiffCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ajmalab/demo/DependencyDiffCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajmalab.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyDiffCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
