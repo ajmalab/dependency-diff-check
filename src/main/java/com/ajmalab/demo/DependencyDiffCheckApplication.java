07259bb9aa9cbce05e5a0f9bef522b599eb4caba
Ajmal Kottilingal <ajmal.kottilingal@transferwise.com>
2023-01-30 18:07:54 +0000

    hello

--- src/main/java/com/ajmalab/demo/DependencyDiffCheckApplication.java
+++ src/main/java/com/ajmalab/demo/DependencyDiffCheckApplication.java
@@ -8,6 +8,7 @@ public class DependencyDiffCheckApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DependencyDiffCheckApplication.class, args);
+		System.out.println("hello");
 	}
 
 }
